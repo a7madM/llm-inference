51eb8af802475dd819ee7b4c1b9c8c07c831709f
Ahmed Magdy <ahmadmgdi94@gmail.com>
2025-10-19 14:46:18 +0200

    Enhance logs

--- handlers/verifier.go
+++ handlers/verifier.go
@@ -18,19 +18,19 @@ func (h *Handler) Verifier(c *fiber.Ctx) error {
 	}
 
 	// Log request
-	fmt.Printf("Enhancing %s with type %s\n", input.Entity, input.Type)
+	fmt.Printf("Verifying %s with type %s\n", input.Entity, input.Type)
 
 	startTime := time.Now()
 
 	// Verify entity
 	result, err := h.entityVerifier.Verify(input.Entity, input.Type)
 	if err != nil {
-		fmt.Printf("Error enhancing entities: %v\n", err)
+		fmt.Printf("Error verifying entities: %v\n", err)
 		return c.Status(fiber.StatusInternalServerError).JSON(models.ErrorResponse{Error: "Failed to enhance entities"})
 	}
 
 	elapsed := time.Since(startTime)
-	fmt.Printf("Entity enhancement completed in %.2f seconds, and results -> %v\n", elapsed.Seconds(), result)
+	fmt.Printf("Entity Verification completed in %.2f seconds, and results -> %v\n", elapsed.Seconds(), result)
 
 	return c.Status(fiber.StatusOK).JSON(result)
 }
